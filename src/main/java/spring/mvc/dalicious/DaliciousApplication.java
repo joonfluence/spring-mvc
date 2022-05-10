12e811d1e1f8aedb6412e19b320a2bbe7fdd7fc3
joonho lee <goodfluence@naver.com>
2022-05-10 22:23:43 +0900

    initial commit

---
+++ src/main/java/spring/mvc/dalicious/DaliciousApplication.java
@@ -0,0 +1,13 @@
+package spring.mvc.dalicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaliciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/mvc/dalicious/DaliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mvc.dalicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
