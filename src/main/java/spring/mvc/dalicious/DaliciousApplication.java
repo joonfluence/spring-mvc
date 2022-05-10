6b4f869a83e413456d613017792748d23fe40c33
joonho lee <goodfluence@naver.com>
2022-05-10 23:09:38 +0900

    template engine setting

--- src/main/java/spring/mvc/dalicious/DaliciousApplication.java
+++ src/main/java/spring/mvc/dalicious/DaliciousApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DaliciousApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DaliciousApplication.class, args);
 	}
-
 }
