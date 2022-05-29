bc4a61cc2bb4bf92059fcb8120ebb21615fd44bb
joonho lee <goodfluence@naver.com>
2022-05-29 17:22:15 +0900

    add timetracing logic to Service

--- src/main/java/spring/mvc/dalicious/service/MemberService.java
+++ src/main/java/spring/mvc/dalicious/service/MemberService.java
@@ -20,11 +20,19 @@ public class MemberService {
 
     // 회원가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원은 받지 않음.
-        // find 후, 중복된 ID가 있으면 에러처리.
-        validateDuplicateName(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            // 같은 이름이 있는 중복 회원은 받지 않음.
+            // find 후, 중복된 ID가 있으면 에러처리.
+            validateDuplicateName(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateName(Member member) {
@@ -37,7 +45,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
